5bc0093d12360056524fd9d10ccef3185aefde9c
Shambhavi Thakur <97224152+codingercat@users.noreply.github.com>
2023-07-31 03:27:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.querySelectorAll("img")[1].setAttribute("src", str2);
 if(randomNumber1>randomNumber2)
    document.querySelector("h1").innerHTML = "🚩Player 1 wins";
 else if(randomNumber1<randomNumber2)
-    document.querySelector("h1").innerHTML = "Payer 2 wins🚩";
+    document.querySelector("h1").innerHTML = "Player 2 wins🚩";
 else
     document.querySelector("h1").innerHTML = "Draw 🤓";
 
